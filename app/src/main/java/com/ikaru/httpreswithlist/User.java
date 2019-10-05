6b356b16bdee55dfc3c6b251e5fb3e58bd9060e8
ikaru19 <syafrizalizal17@gmail.com>
2019-10-05 10:56:26 +0700

    init

---
+++ app/src/main/java/com/ikaru/httpreswithlist/User.java
@@ -0,0 +1,42 @@
+package com.ikaru.httpreswithlist;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+public class User {
+
+    @SerializedName("id")
+    @Expose
+    private String id;
+    @SerializedName("real_name")
+    @Expose
+    private String realName;
+    @SerializedName("name")
+    @Expose
+    private String name;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getRealName() {
+        return realName;
+    }
+
+    public void setRealName(String realName) {
+        this.realName = realName;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+}
